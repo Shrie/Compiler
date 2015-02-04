59104e98b734c7fc363fa0508aa02d7e2d46b6a5
Hunter <unknown-user@fe80::5e7:360:58f2:84c2%17>
2015-02-04 16:51:30 -0700

    scanner start?

--- Compilers/Scanner.cs
+++ Compilers/Scanner.cs
@@ -6,7 +6,12 @@ namespace Compilers
 	{
 		public static void ScannerReader (string x)
 		{
-			Console.WriteLine (x);
+			//Console.WriteLine (x); //for testing
+			for (int i = 0; i < x.Length; i++)
+				using (StringReader sr = new StringReader (x)) {
+					sr.Read(checked, 0, 1);
+
+				}
 		}
 	}
 }
