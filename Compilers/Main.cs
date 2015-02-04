03e06acd189d37f21106d86309d0eb137b1e701f
Hunter Roberts Oehrtman <oehrtmanhu@gmail.com>
2015-02-04 16:19:13 -0700

    Initial check-in of module Compilers

---
+++ Compilers/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Compilers
+{
+	public class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string contents = File.ReadAllText(@"c:\users\Hunter\Desktop\compilers\test1.txt");
+			Scanner.ScannerReader (contents);
+		}
+	}
+}
+
---
+++ Compilers/Scanner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Compilers
+{
+	public class Scanner
+	{
+		public static void ScannerReader (string x)
+		{
+			Console.WriteLine (x);
+		}
+	}
+}
+
