4c44c46e1c38c0c46a30f06b38d1818259650b36
Austo89 <austo89@gmail.com>
2015-02-05 09:01:46 -0700

    working int, fixed, and float scanners

--- Compilers/Scanner.cs
+++ Compilers/Scanner.cs
@@ -6,40 +6,12 @@ namespace Compilers
 {
 	public class Scanner
 	{
+		//dispatcher method destroys whitespace and sends strings to the scanner
 		public static string Dispatcher (string x)
 		{
 			//a string and StringBuilder to build the output token string
 			string tokens;
 			StringBuilder tokenizer = new StringBuilder ();
-
-			//unicode test
-			//int bunyuns = (int)'a';
-			//Console.Write (bunyuns);
-
-			//psuedo code for dispatcher
-			//Console.WriteLine (x); //for testing
-			//Console.WriteLine (x.Length);
-			/* Sudo code
-				 * 
-				 * scan_next_char
-				 * 
-				 * if(char_scanned.isWhiteSpace())
-				 * {
-				 * 		i++
-				 * }
-				 * else
-				 * {
-				 * 		while(char_scanned.isNotWhiteSpace())
-				 * 		{
-				 * 			y += char_scanned
-				 * 			i++
-				 * 		}
-				 * }
-				 * if(y.length != 0)
-				 * {
-				 * 		Scanner(String y)
-				 * }
-				 */
 
 			//disposable stringreader to process the input string
 			using (StringReader win = new StringReader (x)) 
@@ -78,11 +50,16 @@ namespace Compilers
 					}
 				}
 			}
+			//append EOF
+			tokenizer.Append ("MP_EOF");
+
 			//move StringBuilder content to output string
 			tokens = tokenizer.ToString ();
 			return tokens;
 		}
 
+
+		//this method is called by Dispatcher to determine what tokens are in a string
 
 		public static string Scan(string y){
 			//output string
@@ -91,37 +68,132 @@ namespace Compilers
 
 			//read the input string with StringReader
 			using (StringReader check = new StringReader (y)) {
+				//IMPORTANT!
 				//you can find the unicode table online to get char decimal values
 
-				//store next char's decimal value in comp
-				int comp = check.Read ();
+				bool white_space = false;
+				while (!white_space) {
 
-				//check if first char is A-Z,a-z, or _
-				//if it is, it's an identifier
-				if((comp >= 65 && comp <= 90)||(comp >= 97 && comp <= 122)||(comp == 95)){
-					//grab the rest of the identifier in while loop
-					bool is_id = true;
-					while (is_id) {
-						//see if the next char is also part of the identifier
-						int comp2 = check.Peek ();
-						if ((comp2 >= 65 && comp2 <= 90) || (comp2 >= 97 && comp2 <= 122) || (comp2 == 95) || (comp2 >= 48 && comp <= 57)) {
-							//consume the next char
-							int junk = check.Read ();
-						} else {
-							build.Append ("MP_IDENTIFIER ");
-							is_id = false;
+					//store next char's decimal value in comp
+					int comp = check.Read ();
+
+					//check if first char is A-Z,a-z, or _
+					//if it is, it's an identifier
+					if((comp >= 65 && comp <= 90)||(comp >= 97 && comp <= 122)||(comp == 95)){
+
+						//grab the rest of the identifier in while loop
+						bool is_id = true;
+						while (is_id) {
+							//see if the next char is also part of the identifier
+							int comp2 = check.Peek ();
+							if ((comp2 >= 65 && comp2 <= 90) || (comp2 >= 97 && comp2 <= 122) || (comp2 == 95) || (comp2 >= 48 && comp <= 57)) {
+								//consume the next char
+								int junk = check.Read ();
+							} else {
+								//reached the end of the id, return id token and exit the loop
+								build.Append ("MP_IDENTIFIER ");
+								is_id = false;
+							}
 						}
 					}
-				}
-				else if(false){
-					//fill with other scans
-				}
-				else if(false){
 
-				}
-				else{
-					//character unrecognized, append error to string
-					build.Append ("MP_ERROR ");
+					//check if it begins with a decimal
+					//if yes, then it is either a int, fixed, or float
+					else if(comp >= 48 && comp <= 57){
+
+						//grab the rest of the number in a while loop
+						bool is_num = true;
+						bool is_fixed = false;
+						bool is_float = false;
+						while (is_num) {
+							//see if the next char is also part of the number
+							int comp2 = check.Peek ();
+							if (comp2 >= 48 && comp2 <= 57) {
+								//consume the next char
+								int junk = check.Read ();
+							}
+							//if it's a decimal point it could be a fixed or float
+							else if(comp2 == 46) {
+								if (is_fixed) {
+									build.Append ("MP_ERROR ");
+									is_num = false;
+								} else {
+									is_fixed = true;
+								}
+								int junk = check.Read ();
+
+								//make sure the next char is a digit
+								//otherwise it's an error
+								int err_spot = check.Peek ();
+								if (err_spot >= 48 && err_spot <= 57) {
+
+								} else {
+									build.Append ("MP_ERROR ");
+									is_num = false;
+								}
+							}
+							//if it's an e or E, then it's a float
+							else if(comp2 == 69 || comp2 == 101){
+								if (is_float) {
+									build.Append ("MP_ERROR ");
+									is_num = false;
+								} else {
+									is_float = true;
+								}
+								int junk = check.Read ();
+
+								//make sure the next char is a digit or minus sign
+								int err_spot = check.Peek ();
+								if (err_spot == 45){
+									//see if the minus sign is followed by a digit
+									int junk2 = check.Read ();
+									int err_spot2 = check.Peek ();
+									if (err_spot2 >= 48 && err_spot2 <= 57) {
+
+									} else {
+										build.Append ("MP_ERROR ");
+										is_num = false;
+									}
+								}
+								else if (err_spot >= 48 && err_spot <= 57) {
+
+								} else {
+									build.Append ("MP_ERROR ");
+									is_num = false;
+								}
+							}
+							else {
+								//reached the end of the number, check what kind of
+								//number it is based on the flags
+
+								if (is_float) {
+									build.Append ("MP_FLOAT_LIT ");
+								} else if (is_fixed) {
+									build.Append ("MP_FIXED_LIT ");
+								} else {
+									build.Append ("MP_INTEGER_LIT ");
+								}
+
+								is_num = false;
+							}
+						}
+
+					}
+					else if(false){
+
+						//OTHER SCANNERS GO HERE
+
+					}
+					else{
+						//character unrecognized, append error to string
+						build.Append ("MP_ERROR ");
+					}
+
+					white_space = Char.IsWhiteSpace ((char)check.Peek ());
+					if (check.Peek () == -1) {
+						white_space = true;
+					}
+
 				}
 			}
 
@@ -129,6 +201,8 @@ namespace Compilers
 			return process;
 		}
 
+
+		//method to check if an identifier is a reserved word
 		public static string reserved_word(string y){
 			string process = " ";
 
