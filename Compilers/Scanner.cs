010464310e9dc986a990be78be9b227797ee5177
Austo89 <austo89@gmail.com>
2015-02-13 15:43:21 -0700

    working-ish column counter

--- Compilers/Scanner.cs
+++ Compilers/Scanner.cs
@@ -33,7 +33,7 @@ namespace Compilers
 				while (win.Peek () != -1) {
 					//chomp whitespace until next non empty char
 					if (win.Peek() == 13 ||win.Peek() == 133 ||win.Peek() == -1) {
-						column_counter = 1;
+						column_counter = 0;
 						row_counter++;
 						int junk = win.Read ();
 					}
@@ -45,42 +45,58 @@ namespace Compilers
 					//check for a comment
 					//has to happen in the dispatcher at this point
 					else if (win.Peek () == 123) {
+						int add_count = 0;
+						int row_add = 0;
 						//build the comment into a lexeme
 						StringBuilder lexeme = new StringBuilder ();
 						lexeme.Append ((char)win.Read ());
-						column_counter++;
+						//column_counter++;
+						add_count++;
 
 						bool eoc = false;
 						bool err_flag = false;
 						while (!eoc) {
+
 							int comp2 = win.Peek ();
 							if (comp2 == 125) {
 								lexeme.Append ((char)win.Read ());
 								eoc = true;
-								column_counter++;
+								//column_counter++;
+
+							} else if(comp2 == 13 ||comp2 == 133 |comp2 == -1) {
+								lexeme.Append ((char)win.Read ());
+								add_count = 0;
+								row_add++;
 							} else if (comp2 == -1) {
 
 								tokenizer.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_RUN_COMMENT", lexeme.ToString (), row_counter, column_counter, "\n"));
 								eoc = true;
 								err_flag = true;
+								column_counter += add_count;
 							} else if (Char.IsWhiteSpace ((char)comp2)) {
 								lexeme.Append ((char)win.Read ());
-								column_counter++;
+								//column_counter++;
+								add_count++;
 							} else {
 								lexeme.Append ((char)win.Read ());
-								column_counter++;
+								//column_counter++;
+								add_count++;
 							}
 
 						}
 						//int junk2 = check.Read();
 						if (!err_flag) {
 							tokenizer.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_COMMENT", lexeme.ToString (), row_counter, column_counter, "\n"));
+							column_counter += add_count;
+							row_counter += row_add;
 						}
 					} else if (win.Peek () == 39) {
+						int add_count = 0;
 						//build the string into a lexeme
 						StringBuilder lexeme = new StringBuilder ();
 						lexeme.Append ((char)win.Read ());
-						column_counter++;
+						//column_counter++;
+						add_count++;
 
 						bool run_flag = false;
 						bool eos = false;
@@ -90,24 +106,31 @@ namespace Compilers
 							if (comp2 == 39) {
 								lexeme.Append ((char)win.Read ());
 								eos = true;
-								column_counter++;
+								//column_counter++;
+								add_count++;
 							} else if (comp2 == 13 || comp2 == 133 || comp2 == -1) {
 
 								tokenizer.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_STRING_RUN", lexeme.ToString (), row_counter, column_counter, "\n"));
 								eos = true;
 								run_flag = true;
+								column_counter += add_count;
+
 							} else if (Char.IsWhiteSpace ((char)comp2) || comp2 == -1) {
 								lexeme.Append ((char)win.Read ());
-								column_counter++;
+								//column_counter++;
+								add_count++;
 							} else {
 								lexeme.Append ((char)win.Read ());
-								column_counter++;
+								//column_counter++;
+								add_count++;
 							}
 
 						}
 						//int junk2 = check.Read();
 						if (!run_flag) {
 							tokenizer.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_STRING_LIT", lexeme.ToString (), row_counter, column_counter, "\n"));
+							column_counter += add_count;
+
 						}
 					}
 					//when a char is found, build a string to feed to a scanner
@@ -189,16 +212,17 @@ namespace Compilers
 
 				bool white_space = false;
 				while (!white_space) {
-
+					int add_count = 0;
 					//store next char's decimal value in comp
 					int comp = check.Read ();
-					column_counter++;
+					add_count++;
 
 					//check if first char is A-Z,a-z, or _
 					//if it is, it's an identifier
 					if ((comp >= 65 && comp <= 90) || (comp >= 97 && comp <= 122) || (comp == 95)) {
 						StringBuilder id_build = new StringBuilder ();
 						id_build.Append ((char)comp);
+
 						//grab the rest of the identifier in while loop
 						bool is_id = true;
 						while (is_id) {
@@ -207,7 +231,8 @@ namespace Compilers
 							if ((comp2 >= 65 && comp2 <= 90) || (comp2 >= 97 && comp2 <= 122) || (comp2 == 95) || (comp2 >= 48 && comp <= 57)) {
 								//consume the next char
 								id_build.Append ((char)check.Read ());
-								column_counter++;
+								//column_counter++;
+								add_count++;
 							} else {
 
 								//check if the id is a reserved word
@@ -218,6 +243,8 @@ namespace Compilers
 								} catch (KeyNotFoundException) {
 									//reached the end of the id, return id token and exit the loop
 									build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_IDENTIFIER", res_check.ToString (), row_counter, column_counter, "\n"));
+									column_counter += add_count;
+									add_count = 0;
 								}
 
 
@@ -244,7 +271,8 @@ namespace Compilers
 							if (comp2 >= 48 && comp2 <= 57) {
 								//append the number to the lexeme
 								lexeme.Append ((char)check.Read ());
-								column_counter++;
+								//column_counter++;
+								add_count++;
 							}
 							//if it's a decimal point it could be a fixed or float
 							else if (comp2 == 46) {
@@ -252,13 +280,15 @@ namespace Compilers
 									//it already contains a decimal, so it's an error
 									build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_ERROR", lexeme.ToString (), row_counter, column_counter, "\n"));
 									is_num = false;
+									column_counter += add_count;
+									add_count = 0;
 								} else {
 									//the number could be a fixed point or float
 									is_fixed = true;
 								}
 								//append the decimal to the lexeme
 								lexeme.Append ((char)check.Read ());
-								column_counter++;
+								//column_counter++;
 
 								//make sure the next char is a digit
 								//otherwise it's an error
@@ -269,6 +299,8 @@ namespace Compilers
 									//not a digit, raise an error flag
 									build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_ERROR", lexeme.ToString (), row_counter, column_counter, "\n"));
 									is_num = false;
+									column_counter += add_count;
+									add_count = 0;
 								}
 							}
 							//if it's an e or E, then it's a float
@@ -278,20 +310,23 @@ namespace Compilers
 									//it's an error
 									build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_ERROR", lexeme.ToString (), row_counter, column_counter, "\n"));
 									is_num = false;
+									column_counter += add_count;
+									add_count = 0;
 								} else {
 									//mark the float flag
 									is_float = true;
 								}
 								//append the e or E to the lexeme
 								lexeme.Append ((char)check.Read ());
-								column_counter++;
+								//column_counter++;
 
 								//make sure the next char is a digit or minus sign
 								int err_spot = check.Peek ();
 								if (err_spot == 45) {
 									//see if the minus sign is followed by a digit
 									lexeme.Append ((char)check.Read ());
-									column_counter++;
+									//column_counter++;
+									add_count++;
 									int err_spot2 = check.Peek ();
 									if (err_spot2 >= 48 && err_spot2 <= 57) {
 										//found a digit, all is well
@@ -299,6 +334,8 @@ namespace Compilers
 										//not a digit, return an error
 										build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_ERROR", lexeme.ToString (), row_counter, column_counter, "\n"));
 										is_num = false;
+										column_counter += add_count;
+										add_count = 0;
 									}
 								}
 								//check if the E is followed by a digit
@@ -308,6 +345,8 @@ namespace Compilers
 									//no digit, no token for you
 									build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_ERROR", lexeme.ToString (), row_counter, column_counter, "\n"));
 									is_num = false;
+									column_counter += add_count;
+									add_count = 0;
 								}
 							} else {
 								//reached the end of the number, check what kind of
@@ -315,10 +354,16 @@ namespace Compilers
 
 								if (is_float) {
 									build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_FLOAT", lexeme.ToString (), row_counter, column_counter, "\n"));
+									column_counter += add_count;
+									add_count = 0;
 								} else if (is_fixed) {
 									build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_FIXED", lexeme.ToString (), row_counter, column_counter, "\n"));
+									column_counter += add_count;
+									add_count = 0;
 								} else {
 									build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_INTEGER", lexeme.ToString (), row_counter, column_counter, "\n"));
+									column_counter += add_count;
+									add_count = 0;
 								}
 								//mark the next symbol as not part of the number
 								//exit the loop
@@ -328,16 +373,22 @@ namespace Compilers
 
 					} else if (comp == 46) {
 						build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_PERIOD", ".", row_counter, column_counter, "\n"));
+						column_counter++;
 					} else if (comp == 44) {
 						build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_COMMA", ",", row_counter, column_counter, "\n"));
+						column_counter++;
 					} else if (comp == 59) {
 						build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_SCOLON", ";", row_counter, column_counter, "\n"));
+						column_counter++;
 					} else if (comp == 40) {
 						build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_LPAREN", "(", row_counter, column_counter, "\n"));
+						column_counter++;
 					} else if (comp == 41) {
 						build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_RPAREN", ")", row_counter, column_counter, "\n"));
+						column_counter++;
 					} else if (comp == 61) {
 						build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_EQUAL", "=", row_counter, column_counter, "\n"));
+						column_counter++;
 					} else if (comp == 62) {
 						int i62 = check.Peek ();
 						if (i62 == 61) {
@@ -364,23 +415,29 @@ namespace Compilers
 						int i58 = check.Peek ();
 						if (i58 == 61) {
 							int junk = check.Read ();
-							column_counter++;
 							build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_ASSIGN", ":=", row_counter, column_counter, "\n"));
+							column_counter += 2;
 						} else {
 							build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_COLON", ":", row_counter, column_counter, "\n"));
+							column_counter++;
 						}
 					} else if (comp == 43) {
 						build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_PLUS", "+", row_counter, column_counter, "\n"));
+						column_counter++;
 					} else if (comp == 47) { 
 						build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_DIVIDE", "\\", row_counter, column_counter, "\n"));
+						column_counter++;
 					} else if (comp == 45) {
 						build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_MINUS", "-", row_counter, column_counter, "\n"));
+						column_counter++;
 					} else if (comp == 42) {
 						build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_TIMES", "*", row_counter, column_counter, "\n"));
+						column_counter++;
 					} else {
 						//Console.Write (comp);
 						//character unrecognized, append error to string
 						build.Append (String.Format ("{0,-16}{1,-20}{2,-5}{3,-5}{4,0}", "MP_ERROR", (char)comp, row_counter, column_counter, "\n"));
+						column_counter++;
 					}
 
 					white_space = Char.IsWhiteSpace ((char)check.Peek ());
@@ -394,12 +451,6 @@ namespace Compilers
 			process = build.ToString ();
 			return process;
 		}
-
-
 	}
-
-
-
-
 }
 
